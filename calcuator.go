2bb3f98e9ddc612a6d00fd5316ed1e061809545b
Corbyn Ledbetter <corbynledbetter@my.unt.edu>
2024-02-01 13:57:10 -0600

    fixes issues

--- calcuator.go
+++ calcuator.go
@@ -6,41 +6,44 @@ import (
 )
 
 func add(x int, y int) int {
-	fmt.Scan(&x, &y)
 	return x + y
 }
 
 func div(x int, y int) int {
-	fmt.Scan(&x, &y)
 	return x / y
 }
 
 func mulitply(x int, y int) int {
-	fmt.Scan(&x, &y)
 	return x * y
 }
 
 func subtract(x int, y int) int {
-	fmt.Scan(&x, &y)
 	return x - y
 }
 
 func main() {
-	var x int
-	var y int
+	var x, y int
 
 	var operator string
 	fmt.Print("Enter Operator: *, -, +, /: ")
 	fmt.Scan(&operator)
 	switch operator := runtime.GOOS; operator {
 	case "+":
-		fmt.Print("What numbers would you like to add?", add(x, y))
+		fmt.Print("What numbers would you like to add?: ")
+		fmt.Scan(&x, &y)
+		fmt.Print("Result: ", add(x, y))
 	case "-":
-		fmt.Print()
+		fmt.Print("What numbers would you like to subtract: ")
+		fmt.Scan(&x, &y)
+		fmt.Print("Result: ", subtract(x, y))
 	case "*":
-		fmt.Print()
+		fmt.Print("What numbers would you like to muiltply: ")
+		fmt.Scan(&x, &y)
+		fmt.Print("Result: ", mulitply(x, y))
 	case "/":
-		fmt.Print()
+		fmt.Print("What numbers would you like to divide: ")
+		fmt.Scan(&x, &y)
+		fmt.Print("Result,", div(x, y))
 	default:
 		fmt.Print("Invailed Operator. Try again.")
 	}
